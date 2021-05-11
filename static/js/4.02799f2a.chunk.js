f3414cf2b7b43d1d69e6a226f068f5352e3ee140
Anastasiia Boiko <nastasyah86@gmail.com>
2021-05-11 17:24:54 +0300

    Updates

---
+++ static/js/4.02799f2a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp2_2_homework=this.webpackJsonp2_2_homework||[]).push([[4],{126:function(e,s,c){"use strict";c.r(s);c(0);var a=c(2);s.default=function(){return Object(a.jsx)("div",{children:Object(a.jsx)("h1",{className:"welcome",children:"Save all your contacts easy and safe!"})})}}}]);
+//# sourceMappingURL=4.02799f2a.chunk.js.map
